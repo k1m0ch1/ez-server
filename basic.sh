ac77ef66e15fae6a9f85b79e81793ce7332457bb
Yahya Fadhlulloh Al-Fatih <yahya.kimochi@gmail.com>
2018-02-10 01:59:24 +0700

    yah just simple question

--- basic.sh
+++ basic.sh
@@ -1,75 +1,85 @@
 #!/bin/bash
 
 echo "== run this bash script as root"
+echo -n "== this script is running under directory of /root/ez-server, is this right ? (y/n)"
 
-apt-get update
+read answer
+if echo "$answer" | grep -iq "^y" ;then
+    apt-get -y update
 
-apt-get install git build-essential libpcre3 libpcre3-dev libssl-dev libtool autoconf apache2-dev libxml2-dev libcurl4-openssl-dev automake pkgconf htop curl unzip
+	apt-get -y install git build-essential libpcre3 libpcre3-dev libssl-dev libtool autoconf apache2-dev libxml2-dev libcurl4-openssl-dev automake pkgconf htop curl unzip
 
-cd /tmp
+	cd /tmp
 
-mkdir nginx
+	mkdir nginx
 
-wget https://nginx.org/download/nginx-1.13.8.tar.gz -P nginx/
+	wget https://nginx.org/download/nginx-1.13.8.tar.gz -P nginx/
 
-cd /tmp/nginx
+	cd /tmp/nginx
 
-tar -zxvf nginx-1.13.8.tar.gz && rm -f nginx-1.13.8.tar.gz
+	tar -zxvf nginx-1.13.8.tar.gz && rm -f nginx-1.13.8.tar.gz
 
-groupadd -r nginx
-useradd -r -g nginx -s /sbin/nologin -M nginx
+	groupadd -r nginx
+	useradd -r -g nginx -s /sbin/nologin -M nginx
 
-cd /tmp/nginx/nginx-1.13.8
+	cd /tmp/nginx/nginx-1.13.8
 
-./configure --user=nginx --group=nginx --sbin-path=/usr/sbin --modules-path=/usr/lib/nginx --with-http_ssl_module --with-http_gzip_static_module --with-file-aio --with-http_v2_module --with-http_realip_module --without-http_autoindex_module --without-http_browser_module --without-http_geo_module --without-http_userid_module --without-http_memcached_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --without-http_split_clients_module --without-http_uwsgi_module --without-http_scgi_module --without-http_upstream_ip_hash_module --with-http_sub_module --with-http_gunzip_module --with-http_secure_link_module --with-threads --with-stream --with-stream_ssl_module --prefix=/etc/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid --with-cc-opt="-fPIC -I /usr/include/apr-1"
+	./configure --user=nginx --group=nginx --sbin-path=/usr/sbin --modules-path=/usr/lib/nginx --with-http_ssl_module --with-http_gzip_static_module --with-file-aio --with-http_v2_module --with-http_realip_module --without-http_autoindex_module --without-http_browser_module --without-http_geo_module --without-http_userid_module --without-http_memcached_module --without-mail_pop3_module --without-mail_imap_module --without-mail_smtp_module --without-http_split_clients_module --without-http_uwsgi_module --without-http_scgi_module --without-http_upstream_ip_hash_module --with-http_sub_module --with-http_gunzip_module --with-http_secure_link_module --with-threads --with-stream --with-stream_ssl_module --prefix=/etc/nginx --conf-path=/etc/nginx/nginx.conf --http-log-path=/var/log/nginx/access.log --error-log-path=/var/log/nginx/error.log --pid-path=/var/run/nginx.pid --with-cc-opt="-fPIC -I /usr/include/apr-1"
 
-make -j4 install
+	make -j4 install
 
-cd /var/log/nginx
+	cd /var/log/nginx
 
-touch access.log
-touch error.log
+	touch access.log
+	touch error.log
 
-cd /etc/nginx/html/
+	cd /etc/nginx/html/
 
-mkdir /etc/nginx/conf.d
+	mkdir /etc/nginx/conf.d
 
-cp /root/ez-server/conf/nginx/nginx.conf /etc/nginx/nginx.conf
-cp /root/ez-server/conf/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
+	cp /root/ez-server/conf/nginx/nginx.conf /etc/nginx/nginx.conf
+	cp /root/ez-server/conf/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf
 
-mv /etc/nginx/html/index.html /etc/nginx/html/backupindex
+	mv /etc/nginx/html/index.html /etc/nginx/html/backupindex
 
-cp /root/ez-server/conf/nginx/html/index.html /etc/nginx/html/index.html
+	cp /root/ez-server/conf/nginx/html/index.html /etc/nginx/html/index.html
 
-echo "<?php phpinfo(); ?>" > info.php
+	echo "<?php phpinfo(); ?>" > info.php
 
-apt-get install -y python-software-properties; add-apt-repository -y ppa:ondrej/php; apt-get update -y; apt-cache pkgnames | grep php7.1
+	apt-get install -y python-software-properties; add-apt-repository -y ppa:ondrej/php; apt-get update -y; apt-cache pkgnames | grep php7.1
 
-apt-get install -y php7.1 php7.1-fpm php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-xmlrpc php7.1-mysql php7.1-mcrypt php7.1-zip php7.1-curl
+	apt-get install -y php7.1 php7.1-fpm php7.1-cli php7.1-common php7.1-mbstring php7.1-gd php7.1-intl php7.1-xml php7.1-xmlrpc php7.1-mysql php7.1-mcrypt php7.1-zip php7.1-curl
 
-cd /etc/php/7.1/fpm/pool.d/
+	cd /etc/php/7.1/fpm/pool.d/
 
-rm www.conf
-cp /root/ez-server/conf/php/7.1/fpm/pool.d/default.conf /etc/php/7.1/fpm/pool.d/default.conf
+	rm www.conf
+	cp /root/ez-server/conf/php/7.1/fpm/pool.d/default.conf /etc/php/7.1/fpm/pool.d/default.conf
 
-cd /tmp/
+	cd /tmp/
 
-curl -sS https://getcomposer.org/installer -o composer-setup.php
+	curl -sS https://getcomposer.org/installer -o composer-setup.php
 
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+	php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
-nginx
-nginx -s reload
-/etc/init.d/php7.1-fpm restart
+	nginx
+	nginx -s reload
+	/etc/init.d/php7.1-fpm restart
 
-apt-get install mysql-server
-mysql_secure_installation
-systemctl status mysql.service
+	apt-get -y install mysql-server
+	mysql_secure_installation
+	systemctl status mysql.service
 
-cd /etc/nginx/html
+	cd /etc/nginx/html
 
-wget https://files.phpmyadmin.net/phpMyAdmin/4.7.7/phpMyAdmin-4.7.7-all-languages.zip
+	wget https://files.phpmyadmin.net/phpMyAdmin/4.7.7/phpMyAdmin-4.7.7-all-languages.zip
 
-unzip phpMyAdmin-4.7.7-all-languages.zip
+	unzip phpMyAdmin-4.7.7-all-languages.zip 
 
-mv phpMyAdmin-4.7.7-all-languages pm4-pUb
+	rm phpMyAdmin-4.7.7-all-languages.zip
+
+	mv phpMyAdmin-4.7.7-all-languages pm4-pUb
+
+else
+    echo "exit"
+fi
+
